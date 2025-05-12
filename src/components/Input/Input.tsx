340d03c8b3053711380baaede70aa9f6e6365246
leedahye2001 <leedahye2001@naver.com>
2025-05-12 16:30:48 +0900

    refactor: input 컴포넌트 내에 useEffect 제거

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,24 +1,15 @@
-import { useEffect, useState } from "react";
-
 interface InputProps {
   _value?: string;
   _onChange: (value: string | null) => void;
 }
 
 const Input = ({ _value = "", _onChange }: InputProps) => {
-  const [inputValue, setInputValue] = useState<string>(_value);
-
-  useEffect(() => {
-    setInputValue(_value);
-  }, [_value]);
-
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    setInputValue(value);
 
     if (value === "") {
       _onChange(null);
-    } else if (!isNaN(Number(value))) {
+    } else if (/^\d+$/.test(value)) {
       _onChange(value);
     } else {
       alert("숫자를 입력해주세요.");
@@ -31,7 +22,7 @@ const Input = ({ _value = "", _onChange }: InputProps) => {
       <div className="grid mb-8">
         <input
           type="text"
-          value={_value || ""}
+          value={_value}
           onChange={handleChange}
           className="text-[14px] focus-within:bg-[#f1f1f1] ring-1 ring-black text-end"
         />
